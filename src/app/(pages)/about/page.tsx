bb9333c9370c9d67f3db71372e8fee93ede4779b
Nicolas Nagano <nicolasjap@hotmail.com>
2024-10-29 12:43:26 -0300

    feat: add page about

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -4,29 +4,46 @@ import Image from "next/image";
 export default function About() {
   return (
     <>
-      <Banner />
-      <article className="flex flex-col items-center justify-center px-4 py-8 md:px-16 lg:px-32">
-        <h1 className="text-4xl font-bold text-center mt-8 mb-4 text-gray-800">
-          Sobre
-        </h1>
-        <p>História</p>
-        <section className="text-center max-w-2xl mb-8 text-gray-700 leading-relaxed">
-          <p className="text-lg">
+      <Banner imagePath="/images/Rectangle.png" />
+      <article className="flex flex-col items-start px-4 py-8 md:px-16 lg:px-36">
+        <div className="flex flex-col self-center text-center">
+          <h1 className="title mt-8 mb-2">Sobre</h1>
+          <p className="sub-title mb-4">História</p>
+        </div>
+        <section className="text-justify mb-8 leading-relaxed">
+          <p className="mb-4">
             A história do Projeto Sementes do Amanhã começou com três amigos
             que, ao reencontrarem-se na faculdade, lembraram-se de um evento
             marcante que viveram anos antes: uma visita a uma comunidade
             carente, onde conheceram crianças cheias de sonhos, mas sem o apoio
-            necessário para realizá-los.
+            necessário para realizá-los. Tocadas por essa realidade, essas
+            crianças despertaram neles o desejo de fazer algo maior.
+          </p>
+          <p className="mb-4">
+            Com o apoio de familiares e amigos, os três fundadores deram vida ao
+            projeto em 2020. O objetivo sempre foi claro: criar um espaço seguro
+            e acolhedor onde crianças em situação de vulnerabilidade pudessem
+            receber apoio educacional, alimentação e, principalmente, carinho e
+            esperança para o futuro. Ao longo dos anos, o Projeto Sementes do
+            Amanhã cresceu e se tornou um ponto de luz na comunidade, oferecendo
+            atividades educativas, culturais e emocionais que visam transformar
+            vidas.
+          </p>
+          <p className="mb-4">
+            Hoje, graças ao apoio de voluntários e doadores, o projeto já ajudou
+            centenas de crianças, proporcionando um ambiente onde elas podem
+            sonhar e cultivar um futuro melhor. Nossa missão é clara e
+            inabalável: plante hoje a semente do amanhã para colhermos um futuro
+            mais justo e solidário.
           </p>
         </section>
 
-        <div className="flex flex-col md:flex-row items-center gap-8 mt-4">
+        <section className="flex flex-col self-start items-center md:flex-row gap-8 mt-4">
           <Image
             src={"/images/about1.png"}
-            width={400}
-            height={400}
+            width={500}
+            height={500}
             alt="Sobre"
-            className="rounded-lg shadow-lg"
           />
           <div className="max-w-md text-gray-700 leading-relaxed">
             <p>
@@ -37,67 +54,73 @@ export default function About() {
               cada criança pode se tornar agente de sua própria transformação.
             </p>
           </div>
-        </div>
+        </section>
 
-        <section className="w-full mt-12 text-gray-800">
-          <h2 className="text-3xl font-semibold text-center mb-6">
-            Nossa Missão
-          </h2>
-          <p className="text-center max-w-2xl mx-auto leading-relaxed">
+        <section className="mt-12">
+          <h2 className="mb-6">Nossa Missão</h2>
+          <p className="text-start leading-relaxed">
             Promover o desenvolvimento integral das crianças, oferecendo
             recursos para que cada uma possa explorar seu potencial e construir
-            uma trajetória de sucesso.
+            uma trajetória de sucesso. Queremos ser mais do que um suporte
+            temporário; almejamos criar raízes e plantar sementes que floresçam
+            ao longo da vida.
           </p>
         </section>
 
-        <section className="w-full mt-12 text-gray-800">
-          <h2 className="text-3xl font-semibold text-center mb-6">
-            Nossos Valores
-          </h2>
-          <ul className="list-disc list-inside max-w-xl mx-auto text-gray-700 space-y-2">
+        <section className="mt-12">
+          <h2 className=" mb-6">Nossos Valores</h2>
+          <ul className="list-disc list-inside mx-auto text-gray-700 space-y-2">
             <li>
-              Empatia: Cada criança é acolhida com respeito e compreensão.
+              <strong className="font-semibold">Empatia:</strong> Cada criança é
+              acolhida com respeito e compreensão, valorizando suas experiências
+              únicas.
             </li>
             <li>
-              Inclusão: Trabalhamos para criar um espaço onde todas as crianças
-              se sintam pertencentes.
+              <strong className="font-semibold">Inclusão:</strong> Trabalhamos
+              para criar um espaço onde todas as crianças e suas famílias se
+              sintam pertencentes, independentemente de suas origens.
             </li>
             <li>
-              Sustentabilidade: Focamos em programas que geram impacto
-              duradouro.
+              <strong className="font-semibold">Sustentabilidade:</strong>{" "}
+              Nossos programas são pensados para gerar impacto duradouro,
+              focando em educação, habilidades práticas e desenvolvimento
+              emocional.
             </li>
             <li>
-              Comunidade: Incentivamos a participação da comunidade em nossos
-              projetos.
-            </li>
-          </ul>
-        </section>
-
-        <section className="w-full mt-12 text-gray-800">
-          <h2 className="text-3xl font-semibold text-center mb-6">
-            Nossos Programas
-          </h2>
-          <ul className="list-disc list-inside max-w-2xl mx-auto text-gray-700 space-y-2">
-            <li>
-              <strong>Apoio Educacional:</strong> Reforço escolar e
-              desenvolvimento socioemocional.
-            </li>
-            <li>
-              <strong>Cultura e Lazer:</strong> Eventos culturais, oficinas e
-              atividades recreativas.
-            </li>
-            <li>
-              <strong>Assistência Básica:</strong> Acesso a alimentação e
-              suprimentos básicos para as crianças.
+              <strong className="font-semibold">Comunidade:</strong> Acreditamos
+              no poder da coletividade e incentivamos a participação da
+              comunidade em nossos projetos, fortalecendo laços e criando uma
+              rede de apoio.
             </li>
           </ul>
         </section>
 
-        <section className="w-full mt-12 text-gray-800">
-          <h2 className="text-3xl font-semibold text-center mb-6">
-            Junte-se a Nós
-          </h2>
-          <p className="text-center max-w-2xl mx-auto leading-relaxed text-gray-700">
+        <section className="mt-12 text-gray-800">
+          <h2 className="font-bold mb-6">Nossos Programas</h2>
+          <ul className="list-disc list-inside mx-auto text-gray-700 space-y-2">
+            <p>
+              Para alcançar nosso propósito, contamos com uma série de programas
+              desenvolvidos especialmente para apoiar o crescimento das crianças
+              em todas as esferas da vida:
+            </p>
+            <li>
+              <strong className="font-semibold">Apoio Educacional:</strong>{" "}
+              Reforço escolar e desenvolvimento socioemocional.
+            </li>
+            <li>
+              <strong className="font-semibold">Cultura e Lazer:</strong>{" "}
+              Eventos culturais, oficinas e atividades recreativas.
+            </li>
+            <li>
+              <strong className="font-semibold">Assistência Básica:</strong>{" "}
+              Acesso a alimentação e suprimentos básicos para as crianças.
+            </li>
+          </ul>
+        </section>
+
+        <section className="mt-12">
+          <h2 className="mb-6">Junte-se a Nós</h2>
+          <p className="mx-auto leading-relaxed">
             Queremos que você também faça parte dessa jornada de transformação!
             Seja como voluntário, parceiro ou doador, toda contribuição é
             essencial para continuarmos oferecendo um futuro mais justo para
