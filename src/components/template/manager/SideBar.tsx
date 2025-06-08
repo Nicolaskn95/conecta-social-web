88a2c8e45ccb990b5cd399156dca21f2f1740a44
Nicolaskn95 <nicolasjap@hotmail.com>
2025-06-07 22:38:42 -0300

    refactor(SideBar): update sidebar links and icons for improved navigation

--- src/components/template/manager/SideBar.tsx
+++ src/components/template/manager/SideBar.tsx
@@ -1,75 +1,72 @@
 // components/Sidebar.js
-"use client";
+'use client';
 import {
-  House,
-  Users,
-  HandHeart,
-  Calendar,
-  IdentificationCard,
-  SignOut,
-  UserSquare,
-  IdentificationBadge,
-  UsersThree,
-} from "@phosphor-icons/react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+   HouseIcon,
+   UsersIcon,
+   HandHeartIcon,
+   CalendarIcon,
+   SignOutIcon,
+   IdentificationBadgeIcon,
+} from '@phosphor-icons/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Sidebar() {
-  const pathname = usePathname();
-  const sidebarTopics = [
-    {
-      description: "Dashboard",
-      link: "/admin",
-      icon: <UserSquare size={24} />,
-    },
-    {
-      description: "Famílias",
-      link: "/admin/families",
-      icon: <UsersThree size={24} />,
-    },
-    {
-      description: "Doações",
-      link: "/admin/donations",
-      icon: <HandHeart size={24} />,
-    },
-    {
-      description: "Eventos",
-      link: "/admin/events",
-      icon: <Calendar size={24} />,
-    },
-    {
-      description: "Administrador",
-      link: "/admin/managers",
-      icon: <IdentificationBadge size={24} />,
-    },
-  ];
+   const pathname = usePathname();
+   const sidebarTopics = [
+      {
+         description: 'Dashboard',
+         link: '/dashboard',
+         icon: <HouseIcon size={24} />,
+      },
+      {
+         description: 'Famílias',
+         link: '/dashboard/families',
+         icon: <UsersIcon size={24} />,
+      },
+      {
+         description: 'Doações',
+         link: '/dashboard/donations',
+         icon: <HandHeartIcon size={24} />,
+      },
+      {
+         description: 'Eventos',
+         link: '/dashboard/events',
+         icon: <CalendarIcon size={24} />,
+      },
+      {
+         description: 'Administrador',
+         link: '/dashboard/managers',
+         icon: <IdentificationBadgeIcon size={24} />,
+      },
+   ];
 
-  return (
-    <aside className="sidebar-color w-64 h-auto flex flex-col justify-between p-4">
-      <div>
-        {sidebarTopics.map((topic, index) => {
-          const isActive =
-            topic.link === "/admin"
-              ? pathname === topic.link
-              : pathname.startsWith(topic.link);
-          return (
-            <Link
-              key={index}
-              href={topic.link}
-              className={`sidebar-menu flex items-center gap-2 p-2 mb-4 cursor-pointer rounded-lg ${
-                isActive ? "sidebar-menu-selected" : ""
-              }`}
-            >
-              {topic.icon}
-              {topic.description}
-            </Link>
-          );
-        })}
-      </div>
-      <div className="flex items-center gap-2 p-2 hover:bg-[#A13838] hover:text-white text-[#387AA1] cursor-pointer rounded-lg">
-        <SignOut size={24} />
-        Logout
-      </div>
-    </aside>
-  );
+   return (
+      <aside className="sidebar-color w-64 h-auto flex flex-col justify-between p-4">
+         <div>
+            {sidebarTopics.map((topic, index) => {
+               const isActive =
+                  topic.link === '/dashboard'
+                     ? pathname === topic.link
+                     : pathname.startsWith(topic.link);
+               return (
+                  <Link
+                     key={index}
+                     href={topic.link}
+                     className={`sidebar-menu flex items-center gap-2 p-2 mb-4 cursor-pointer rounded-lg ${
+                        isActive ? 'sidebar-menu-selected' : ''
+                     }`}
+                  >
+                     {topic.icon}
+                     {topic.description}
+                  </Link>
+               );
+            })}
+         </div>
+         <div className="flex items-center gap-2 p-2 hover:bg-[#A13838] hover:text-white text-[#387AA1] cursor-pointer rounded-lg">
+            <SignOutIcon size={24} />
+            Logout
+         </div>
+      </aside>
+   );
 }
